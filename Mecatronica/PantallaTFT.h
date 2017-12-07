daf1d62b6a08c5f31d4f16b0a824c0c7abc8c8b7
Pablo Horno <pablojhorno@gmail.com>
2017-12-07 23:07:41 +0100

    Añadida lectura de bmp

--- Mecatronica/PantallaTFT.h
+++ Mecatronica/PantallaTFT.h
@@ -8,6 +8,8 @@
 #else
 #include "WProgram.h"
 #endif
+#include <SPI.h>
+#include <SD.h>
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Wire.h>
 #include <LM75.h>
@@ -20,6 +22,8 @@
 #define XM A2  // must be an analog pin, use "An" notation!
 #define YM 7   // can be a digital pin
 #define XP 6   // can be a digital pin
+#define SD_CS 5
+
 enum Color
 {
 	Negro = 0x0000,
@@ -52,7 +56,12 @@ public:
 	const int limiteTouchInferior = 180;
 
 	Vector2<int> getTouchPoint();
+	Vector2<int> getTouchPoint(int&);
+	inline bool esPresionSuficiente(const int presion) { return presion>minPresion && presion < maxPresion; };
+	void dibujarBMP(char *filename, int x, int y);
 
+	uint16_t read16(File f);
+	uint32_t read32(File f);
 	
 };
 
